33603fc916ae2f5c3a76ef1527250929a5101c5f
Yashmala <jain.yashmala513@gmail.com>
2024-03-10 14:49:31 +0530

    change in print statement

--- app/src/main/java/com/example/admin/vikram.java
+++ app/src/main/java/com/example/admin/vikram.java
@@ -10,5 +10,6 @@ public class vikram extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vikram);
+        System.out.print(" hello a remote commit");
     }
 }
